a89674dc595bdb005d9ce296931e86099fb9d7b2
Eda Dural <edaadurall@gmail.com>
2023-01-05 23:32:39 +0300

    commit

---
+++ head-tail.sh
@@ -0,0 +1,31 @@
+#/bin/bash!
+
+FILE=`zenity --file-selection --title="Select a File"`
+
+
+case $? in
+	0)
+  
+		
+		ilk_secim=$(zenity --forms --title="Satir secim" \
+			--text="Ilk satir numarasini giriniz." \
+			--separator="," \
+			--add-entry="Ilk satir")
+		
+		son_secim=$(zenity --forms --title="Satir secim" \
+			--text="Son satir numarasini giriniz." \
+			--separator="," \
+			--add-entry="Son satir")
+		
+		aralik=$(expr $son_secim - $ilk_secim)
+
+		head -n$son_secim $FILE | tail -n$aralik
+		
+		zenity	--info \
+			--text="Sonuc terminalde yazilmistir.";;
+
+	1)
+                echo "No file selected.";;
+	-1)
+                echo "An unexpected error has occurred.";;
+esac
